bdf9978575c553582528c0a27cd6c581e2d6c634
Davi Slm <davisoaresdasilva1203@gmail.com>
2024-03-13 16:41:02 -0300

    POO

---
+++ Curso/src/estaticos/circulo.java
@@ -0,0 +1,23 @@
+package estaticos;
+
+import java.util.Scanner;
+
+public class circulo {
+    public static final double PI = 3.14;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Dite o radianos: ");
+        double radius = sc.nextDouble();
+
+        double c = cincuferencia(radius);
+
+        double v = volume(radius);
+
+        System.out.printf("Circunferência: ", c);
+        System.out.printf("Volume: ", v);
+
+
+    }
+}
